f5fd786b9ef2092c50e37715ef7d2c908a38c362
StudentBlossom <127415623+StudentBlossom@users.noreply.github.com>
2024-07-05 17:35:35 +0200

    Poppop
    first few lines

---
+++ ML/ML/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+
+
+Console.WriteLine("Welcome");
+Console.WriteLine("");
+
+Timer tijd = new Timer(repeater, null, 0, 15);//this makes it so that the function repeater gets called every 15ms
+void repeater(object o)
+{
+
+}
+
+
+uint multigame = 1;//how many games run at the same time!
+byte[,,] gamestate = new byte[multigame,10,10];
+/*
+ 0 = empty .
+1 = player P
+2 = 
+  made multiple to support multiple games at the same time
+  
+  
+ */
+
+
+
+//
+
+
+
+
+//copyright owned by BlossomStudio

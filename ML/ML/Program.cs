617804078aa69ed72675fcc12d87400b4394dbe3
StudentBlossom <127415623+StudentBlossom@users.noreply.github.com>
2024-07-10 14:46:33 +0200

    More basics added
    vooruitgang aan de basis van het spel laten zien, bytemask verbetered om dynamisch te werken

--- ML/ML/Program.cs
+++ ML/ML/Program.cs
@@ -1,14 +1,28 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 using System.Threading;
 
 
 Console.WriteLine("Welcome");
 Console.WriteLine("");
 
+int playerY = 4;
+void testfuncup () {
+    playerY++;
+}
+void testfuncdown () {
+    playerY--;
+}
+
+Action[] ML_output = {testfuncup, testfuncdown,  testfuncdown, testfuncdown, testfuncdown, testfuncdown };
+int ML_output_size = ML_output.Length;
+int ML_input_size = 6;//max is 8
+int ML_input_size_sq = (byte)Math.Pow(2, ML_input_size);
+//this  does not work for multiple gamesets... yet
 
 
 uint multigame = 3;//how many games run at the same time!
-uint gamesize = 10;
+uint gamesize = 7;//
 byte[,,] gamestate = new byte[multigame,gamesize,gamesize];
 
 
@@ -31,88 +45,33 @@ made multiple to support multiple games at the same time
  */
 
 
-
-// array aan dictionaries
-// x,y,x-y,xy er is een constante hoveelheid van omgevingsfactoren die altijd hetzellfde is
-//elke getal in de array staat voor een van die omgevingsfactoren
-//in de dictionariy zoek je de waarde van die factor en daarin staat waarde die onthouden moet worden
-
-string[] teststring = {"a","b","c","d","e"};
-List<string> testlist = new List<string>();
-for (int i = 0; i < 5; i++)
-{
-    testlist.Add(teststring[i]);
-}
-for(int i = 0;  i < 5;i++)
-{
-    for(int ii = i + 1; ii < 5;ii++)    
-    {
-        testlist.Add(teststring[i] + "-" + teststring[ii]);
-    }
-}
-
-
-//input is bytes
-//6 bytes
-//lets say 8 bytes at max
-//long = 8 bytes
-//we can remember the input USING BIT MASK
-//of 1 byte
-// DUS als we werken met 8 inputs 
-//moeten we het getal 0000 0101 opzoeken wat X en Z is!!!!!!!!!
-//en 0000 0111 is XYZ en dat kunnen we gebruiken om een bitmask te generen voor die operatie
-//die voeren we uit op de long die we creeren uit de invoer!!!!
-//we moeten wel alle 8 bytes in een long zetten!
-//we kunnen dit principe ook toepassen op de x-y
-//dan moeten we een long voor alle x-y x-z ff kijken hoe we die doen maar dat kan
-//
-//WE KUNNEN DAN OOK EEN LONG CREEREN VAN X-Y&Z WANT 
-//als we de long van de x nemen en die willen we combineren met
-//de z in de invoerlong hebben we 2 longs
-//de ene is (in bytemask) 0000 0001 voor Z en 0000 0010 voor x-y
-//moeten wel kijken of we die in 2 dictionaries moeten zetten want wat nu als 
-//x-y= een y die al gedefinieerd is?
-
-//alleen probleem als een getal 0 is maar dat kunnen we oplossen
-
-//TODO CODE BYTEMASK met parabyte en paralong
-
-
-
-//we willen heel veel factoren maken
-//DUS we moeten x,y, z x-y, x-z,y-z, xy, xz,yz z x-y, y x-z, x y-z,xyz
-//als basis nemen we x,y,z,x-y,x-z,z-y
-//daarop moeten we alle gecombineerde variabelen nemen. dus xy,xz,yz
-//kunnen we voorkomen dat we xy x-y hebben? geen idee
-
-
-// 1100 1010 0100 1010
-// 1010
-//1100 0000 0100 0000
-//0000 0000 0000 0000
-//1111 0000 0000 0000 >> 0000 1111 0000 0000
-//
 ulong bytemask (byte parabyte, ulong paralong) //see the paralong as an array of bytes, 8 of them, and the parabyte as mask to show which bytes are returned
 {
-    ulong returnme = paralong;
     ulong tempmasklong = 0;
-    for(int i = 0; i < 7; i++)
+    parabyte =(byte) (parabyte<<(8 - ML_input_size));//we get it in format with all the bits centered around the right but we need to check it on the left
+    for (int i = 0; i < ML_input_size-1; i++) //we skipped these steps above
     {
-        if (parabyte -128>=0)//als het meest linkse getal 1 is
+        if (parabyte -128>=0)//als het meest linkse bit 1 is
         {
             tempmasklong = tempmasklong + 255;//voegen we een byte aan 1tjes toe
         }
         tempmasklong = (tempmasklong << 8);//en die bewegen we naar links
         parabyte = (byte)(parabyte << 1);//en de volgende die we moeten bekijken schuiven we nu door
     }
-    if (parabyte - 128 >= 0)//als het meest rechste getal 0 is
+    Console.WriteLine(parabyte);
+    if (parabyte - 128>= 0)//hier checken we de laatste bit. dit doen we niet in de loop want als we dan de tempmasklong doorschuiven klopt die niet meer
     {
-        tempmasklong = tempmasklong + 255;
+        tempmasklong = tempmasklong + 255;//de laatste bit in de maskbyte is de eerste byte in de long
     }
-    return (returnme&tempmasklong);
+    return (paralong&tempmasklong);//we masken de originele en returnen het
+ 
+    // 1100 1010 0100 1010 (de long)
+    // 1010 (de byte)
+    //1100 0000 0100 0000 (de uitkomst)
 }
 
 
+Dictionary<ulong, memory> learned = new Dictionary<ulong, memory>();
 byte x = 0; 
 Timer tijd = new Timer(repeater, null, 0, 1000);//this makes it so that the function repeater gets called every 40ms
 void repeater(object o)
@@ -125,10 +84,7 @@ void repeater(object o)
     ulong testvalue = bytemask(x, ulong.MaxValue);
     Console.WriteLine(Convert.ToString((long)testvalue,2));
     Console.WriteLine(Convert.ToString(x,2));
-    for(int i = 0; i < testlist.Count; i++)
-    {
-        Console.WriteLine(testlist[i]);
-    }
+    Console.WriteLine(ML_output_size);
 
     for (int iy = 0; iy < gamesize; iy++)
     { //y
@@ -158,8 +114,43 @@ void repeater(object o)
 
 Console.ReadLine();
 
+int[] ML_choice_calculator_aid(long paralong) //de paralong is all the inputs combined into 1
+{
+    int[] returnme = new int[ML_output_size];
+
+    for (byte i = 1; i <= ML_input_size_sq; i++) //to check all the code
+    {
+        memory x = learned[bytemask(i, (ulong)paralong)];
+    }
+
+    return returnme;
+}
 
 
+static public class ML_finetune
+{
+    public static uint startvalue = 100;
+
+}
+
+public class memory//check if this is the best way or if I need to change 
+    //the construct class to take a int[] as para so I can change it. idk this is a dictionary quirk
+{
+    public byte maskID;
+    public uint[] values;
+
+    public memory(byte parabyte,ulong paralong,int paraint)
+    {
+         maskID = parabyte;
+        values = new uint[paraint];
+        for(int i = 0; i < paraint; i++)
+        {
+            values[i] = ML_finetune.startvalue;
+        }
+    }
+
+
+}
 
 
 //copyright owned by BlossomStudio
